eb1dca4cff15979f68084ea13064f3cbefbe369d
Carlos Miranda <polomiranda@outlook.com>
2021-10-04 04:19:12 -0300

    add: readme.md

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,7 +3,6 @@ import styled from "styled-components/native";
 export const Container = styled.SafeAreaView`
   background-color: #141a29;
   flex: 1;
-  padding: 4px 0;
 `;
 
 export const SearchContainer = styled.View`
